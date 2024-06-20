d7be8422aa5d1d9d9dcdff2cbf3935a357c4ff6a
Maga <magomed14102006@gmail.com>
2024-06-20 10:51:03 +0300

    У сайта появились стили

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse('<h1>Главная страница</h1>')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Другое</h1>')
+    return render(request, 'main/about.html')
