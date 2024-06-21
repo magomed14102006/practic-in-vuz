e7506aa623f9b38108f56bc40ef67842351c5ed8
Maga <magomed14102006@gmail.com>
2024-06-21 12:12:18 +0300

    Закончил обновление данных

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -4,18 +4,28 @@ import requests
 url = 'https://api.binance.com/api/v3/ticker/price'
 
 
-def price():
-    response = requests.get(url, {"symbol": "BTCUSDT"})
-    data = response.json()
-    data = data["price"]
-    return data
-
-
-result = price()
-
+# def price():
+#     response = requests.get(url, {"symbol": "BTCUSDT"})
+#     data = response.json()
+#     data = data["price"]
+#     return data
+#
+#
+# result = price()
 
 def index(request):
-    return render(request, 'main/index.html', {'data': result})
+    api_url = url
+    response = requests.get(api_url, {"symbol": "BTCUSDT"})
+    if response.status_code == 200:
+        data = response.json()
+        data = data["price"]
+    else:
+        data = {}
+    return render(request, 'main/index.html', {'data': data})
+
+
+# def index(request):
+#     return render(request, 'main/index.html', {'data': result})
 
 
 def about(request):
